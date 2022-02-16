cd6acf6e6e96f3a4542d6f1df88c70319a84b621
Joel Gabriel (Desktop) <joel@inspacexr.com>
2022-02-17 10:58:39 +1100

    Updated

---
+++ precache-manifest.ac2e76e80182965af676e052c1994046.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f0bc327824367a073dfbc1105a0948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01ac58834cf55756c0ca",
+    "url": "/static/css/main.c7e92852.chunk.css"
+  },
+  {
+    "revision": "a33abf5ef437f32b2d9f",
+    "url": "/static/js/2.d0b4b4da.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.d0b4b4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ac58834cf55756c0ca",
+    "url": "/static/js/main.2d8fd77c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "b27885091f4ee14d5356ff5dd988fde4",
+    "url": "/static/media/inspacevr.b2788509.png"
+  },
+  {
+    "revision": "8eea8e15bcbe12fc5e89c69d7dcc8ec2",
+    "url": "/static/media/profile.8eea8e15.jpg"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/static/media/psykinetic.9c1952ca.png"
+  }
+]);
